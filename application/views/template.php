b03c73bf6f292dc83cafb3304c62fc72327a4037
Woody Gilk <woody.gilk@kohanaphp.com>
2009-09-28 12:02:55 -0500

    Updated template, added "develop swiftly" tagline to logo, use image replacement for footer logo

--- application/views/template.php
+++ application/views/template.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo Kohana::$charset ?>" />
-<title>KohanaPHP</title>
+<title>Kohana: <?php echo $title ?></title>
 
 <?php echo
 	HTML::style('media/css/print.css', array('media' => 'print')), "\n",
@@ -16,7 +15,7 @@
 
 <div id="header">
 	<div class="container">
-		<?php echo HTML::anchor('', HTML::image('media/img/kohana.png', array('alt' => 'Kohana')), array('id' => 'logo')) ?>
+		<?php echo HTML::anchor('', HTML::image('media/img/kohana.png', array('alt' => 'Kohana: Develop Swiftly')), array('id' => 'logo')) ?>
 		<div id="menu">
 			<ul>
 				<li class="home first"><?php echo HTML::anchor('', 'Home') ?></li>
@@ -37,34 +36,36 @@
 	</div>
 </div>
 
-<div id="footer" class="container">
-	<div class="copyright span-6 prefix-1">
-		<p class="caps top"><?php echo HTML::anchor('', HTML::image('media/img/kohana_dark.png', array('alt' => 'Kohana'))) ?><br/>
-			Copyright &copy;2007-2009<br/>
-			All rights reserved<br/>
-			The awesome <?php echo HTML::anchor('team', 'Kohana Team') ?>
-		</p>
-	</div>
-	<div class="discussions feed span-8">
-		<h6 class="caps top">Latest Discussions</h6>
-		<ol>
-			<?php echo View::factory('template/feed', array(
-				'feed'  => 'http://forum.kohanaphp.com/search.php?PostBackAction=Search&Advanced=1&Type=Comments&Feed=RSS2',
-				'limit' => 5,
-				'more'  => 'More discussions',
-			)) ?>
-		</ol>
-	</div>
-	<div class="changes feed span-8 suffix-1 last">
-		<h6 class="caps top">Latest Development</h6>
-		<ol>
-			<?php echo View::factory('template/feed', array(
-				'feed'  => 'http://dev.kohanaphp.com/activity.atom?key=yUHDvnjJryqHZmgiSNAaReHU6V5JHXBNnew8gRLu&show_changesets=1',
-				'limit' => 5,
-				'link'  => 'id',
-				'more'  => 'More development',
-			)) ?>
-		</ol>
+<div id="footer">
+	<div class="container">
+		<div class="copyright span-6 prefix-1">
+			<p class="caps top"><?php echo HTML::anchor('', 'Kohana', array('class' => 'logo')) ?>
+				Copyright &copy;2007-2009<br/>
+				All rights reserved<br/>
+				The awesome <?php echo HTML::anchor('team', 'Kohana Team') ?>
+			</p>
+		</div>
+		<div class="discussions feed span-8">
+			<h6 class="caps top">Latest Discussions</h6>
+			<ol>
+				<?php echo View::factory('template/feed', array(
+					'feed'  => 'http://forum.kohanaphp.com/search.php?PostBackAction=Search&Advanced=1&Type=Comments&Feed=RSS2',
+					'limit' => 5,
+					'more'  => 'More discussions',
+				)) ?>
+			</ol>
+		</div>
+		<div class="changes feed span-8 suffix-1 last">
+			<h6 class="caps top">Latest Development</h6>
+			<ol>
+				<?php echo View::factory('template/feed', array(
+					'feed'  => 'http://dev.kohanaphp.com/activity.atom?key=yUHDvnjJryqHZmgiSNAaReHU6V5JHXBNnew8gRLu&show_changesets=1',
+					'limit' => 5,
+					'link'  => 'id',
+					'more'  => 'More development',
+				)) ?>
+			</ol>
+		</div>
 	</div>
 </div>
 
