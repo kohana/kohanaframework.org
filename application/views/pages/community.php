a5d520e106c08b703b5a1182cb54eb3869bcc956
Woody Gilk <woody.gilk@kohanaphp.com>
2009-09-29 20:17:39 -0500

    Added content to community page

--- application/views/pages/community.php
+++ application/views/pages/community.php
@@ -1,3 +1,30 @@
-<div class="span-24">
-	Forums, IRC, Facebook
+<div class="span-22 prefix-1 suffix-1 last">
+
+	<h1>Connect and Share</h1>
+
+	<p class="intro">An open source project is nothing without a community.</p>
+
+	<dl>
+		<dt>Forum</dt>
+		<dd>Official annoucements, community support, and feedback can be found on our <?php echo HTML::anchor('http://forum.kohanaphp.com/', 'community forum') ?>.</dd>
+
+		<dt>IRC</dt>
+		<dd>Chat with follow Kohana users at <?php echo HTML::anchor('irc://irc.freenode.net/kohana', '#kohana on freenode') ?>.</dd>
+
+		<dt>Github</dt>
+		<dd>The easiest way to contribute to v3.x is to <?php echo HTML::anchor('http://github.com/kohana', 'fork us on github') ?>.</dd>
+
+		<dt>Twitter</dt>
+		<dd>Read release annoucements and random things about Kohana by <?php echo HTML::anchor('http://twitter.com/KohanaPHP', 'following us') ?>.</dd>
+
+		<dt>Facebook</dt>
+		<dd>Join the <?php echo HTML::anchor('http://www.facebook.com/group.php?gid=140164501435', 'Facebook group') ?> and find other users.</dd>
+
+		<dt>Ohloh</dt>
+		<dd>View project information and give Kudos for <?php echo HTML::anchor('http://www.ohloh.net/p/ko3', 'v3.x') ?> and <?php echo HTML::anchor('http://www.ohloh.net/p/Kohana', 'v2.x') ?>.</dd>
+
+		<dt>Stack Overflow</dt>
+		<dd>Share your knowledge by answering <?php echo HTML::anchor('http://stackoverflow.com/questions/tagged/kohana', 'user questions') ?> about Kohana.</dd>
+	</dl>
+
 </div>
