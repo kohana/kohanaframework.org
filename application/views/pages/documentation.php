20275cf91a6adc024daf6f021246497af087ded2
Woody Gilk <woody.gilk@kohanaphp.com>
2009-09-29 20:17:52 -0500

    Tweaked documentation page

--- application/views/pages/documentation.php
+++ application/views/pages/documentation.php
@@ -4,8 +4,12 @@
 
 	<p class="intro">Documentation is provided for both v2.x and v3.x, in separate places.</p>
 
-	<p>For documenation of v2.x, please use the <?php echo HTML::anchor('http://docs.kohanaphp.com/', 'Kohana Wiki') ?>.</p>
+	<p>For documenation of v2.x, please use the <?php echo HTML::anchor('http://docs.kohanaphp.com/', 'Kohana Documentation Wiki') ?>.</p>
 
-	<p>Documentation for v3.x can be found <?php echo HTML::anchor(Route::get('docs/guide')->uri(), 'online') ?> and is also included in the <code>userguide</code> module in releases.</p>
+	<p>Documentation for v3.x can be found <?php echo HTML::anchor(Route::get('docs/guide')->uri(), 'online') ?> and is also included in the <code>userguide</code> module in all releases.</p>
+
+	<h2>I still need help!</h2>
+
+	<p>The Kohana <?php echo HTML::anchor('community', 'user community') ?> may be able to help you find the answer you are looking for.</p>
 
 </div>
