c875b7a28ec7cb19231bafd67755693cd15e5c1b
Woody Gilk <woody.gilk@kohanaphp.com>
2009-10-08 18:35:42 -0500

    Updated download page with content

--- application/views/pages/download.php
+++ application/views/pages/download.php
@@ -1,6 +1,49 @@
-<div class="v3 span-12">
-	download v3
+<div class="span-22 prefix-1 suffix-1 last">
+	<h1>Get yours today!</h1>
+
+	<p class="intro">Kohana has a couple different versions to choose from.</p>
+
+	<dl>
+		<dt>Never used Kohana before?</dt>
+		<dd>Try v3.x and see if you like it.</dd>
+
+		<dt>Building a completely new application and expect to launch in 4+ months?</dt>
+		<dd>We recommend v3.x for new applications.</dd>
+
+		<dt>Upgrading an existing Kohana-based application?</dt>
+		<dd>Upgrade to v2.3.x first, then review the changes in v2.4.x.</dd>
+	</dl>
+
 </div>
-<div class="v2 span-12 last">
-	download v2
+<div class="download span-11 prefix-1">
+
+	<h2 class="bottom">v3.0.x <small class="fancy">"renaissance"</small></h2>
+	<p class="intro">Current stable release of the v3.x branch.<br/>
+		<?php echo HTML::anchor('guide', 'Documentation') ?>
+		<?php echo HTML::anchor('http://dev.kohanaphp.com/projects/kohana3/issues', 'Issues') ?>
+	</p>
+	<p class="top">Download: <?php echo HTML::anchor('http://dev.kohanaphp.com/attachments/download/1328/kohana-v3.0.zip', 'v3.0.0.zip') ?></p>
+
+	<h3 class="bottom">v3.1.x</h3>
+	<p><em>No current release, in development.</em><br/>
+		<?php echo HTML::anchor('http://dev.kohanaphp.com/versions/show/130', 'Status') ?>
+	</p>
+
+</div>
+<div class="download span-11 suffix-1 last">
+
+	<h2 class="bottom">v2.3.4 <small class="fancy">"buteo regalis"</small></h2>
+	<p class="intro">Current stable release of the v2.x branch.<br/>
+		<?php echo HTML::anchor('http://docs.kohanaphp.com/', 'Documentation') ?>
+	</p>
+	<p class="top">Download: <?php echo HTML::anchor('http://dev.kohanaphp.com/attachments/download/1355/kohana-v2.3.4.zip', 'v2.3.4.zip') ?><br/>
+		<strong class="caps">Final release of the v2.3.x branch!</strong></p>
+
+	<h3 class="bottom">v2.4.x</h3>
+	<p>Current beta release of the v2.x branch.<br/>
+		<?php echo HTML::anchor('http://dev.kohanaphp.com/versions/show/10', 'Status') ?>
+		<?php echo HTML::anchor('http://dev.kohanaphp.com/projects/kohana2/wiki/24_Roadmap', 'Changes') ?>
+	</p>
+	<p>Download: <?php echo HTML::anchor('http://dev.kohanaphp.com/attachments/download/1349/kohana-2.4b2.zip', 'v2.4b2.zip') ?></p>
+
 </div>
