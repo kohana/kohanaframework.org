8245b108b131e7646f798e3ff73d13d0b610a685
Woody Gilk <woody.gilk@kohanaphp.com>
2009-10-12 12:09:53 -0500

    Updated download page

--- application/views/pages/download.php
+++ application/views/pages/download.php
@@ -1,4 +1,4 @@
-<div class="span-22 prefix-1 suffix-1 last">
+<div id="download" class="span-22 prefix-1 suffix-1 last">
 	<h1>Get yours today!</h1>
 
 	<p class="intro">Kohana has a couple different versions to choose from.</p>
@@ -14,36 +14,44 @@
 		<dd>Upgrade to v2.3.x first, then review the changes in v2.4.x.</dd>
 	</dl>
 
-</div>
-<div class="download span-11 prefix-1">
+	<div class="package span-11">
 
-	<h2 class="bottom">v3.0.x <small class="fancy">"renaissance"</small></h2>
-	<p class="intro">Current stable release of the v3.x branch.<br/>
-		<?php echo HTML::anchor('guide', 'Documentation') ?>
-		<?php echo HTML::anchor('http://dev.kohanaphp.com/projects/kohana3/issues', 'Issues') ?>
-	</p>
-	<p class="top">Download: <?php echo HTML::anchor('http://dev.kohanaphp.com/attachments/download/1328/kohana-v3.0.zip', 'v3.0.0.zip') ?></p>
+		<h2 class="bottom">v3.0.0 <small class="fancy">"renaissance"</small><br/>
+			<small>Current stable release of the v3.x branch.</small></h2>
+		<p>
+			<?php echo HTML::anchor('http://dev.kohanaphp.com/attachments/download/1328/kohana-v3.0.zip', 'Download (zip)') ?>
+			<?php echo HTML::anchor('guide', 'Documentation') ?>
+			<?php echo HTML::anchor('http://dev.kohanaphp.com/projects/kohana3/issues', 'Issues') ?>
+			<?php echo HTML::anchor('http://github.com/kohana/kohana', 'Repository') ?>
+		</p>
 
-	<h3 class="bottom">v3.1.x</h3>
-	<p><em>No current release, in development.</em><br/>
-		<?php echo HTML::anchor('http://dev.kohanaphp.com/versions/show/130', 'Status') ?>
-	</p>
+		<h3 class="bottom">v3.1.0 <small class="warning">unstable</small><br/>
+			<small>Current beta release of the v3.x branch.</small></h3>
+		<p>
+			<?php echo HTML::anchor('http://dev.kohanaphp.com/versions/show/130', 'Status') ?>
+			<?php echo HTML::anchor('http://github.com/kohana/core/tree/unstable', 'Repository') ?>
+		</p>
 
-</div>
-<div class="download span-11 suffix-1 last">
+	</div>
+	<div class="package span-11 last">
 
-	<h2 class="bottom">v2.3.4 <small class="fancy">"buteo regalis"</small></h2>
-	<p class="intro">Current stable release of the v2.x branch.<br/>
-		<?php echo HTML::anchor('http://docs.kohanaphp.com/', 'Documentation') ?>
-	</p>
-	<p class="top">Download: <?php echo HTML::anchor('http://dev.kohanaphp.com/attachments/download/1355/kohana-v2.3.4.zip', 'v2.3.4.zip') ?><br/>
-		<strong class="caps">Final release of the v2.3.x branch!</strong></p>
+		<h2 class="bottom">v2.3.4 <small class="fancy">"buteo regalis"</small> <small class="warning">final</small><br/>
+			<small>Current stable release of the v2.x branch.</small></h2>
+		<p>
+			<?php echo HTML::anchor('http://dev.kohanaphp.com/attachments/download/1355/kohana-v2.3.4.zip', 'Download (zip)') ?>
+			<?php echo HTML::anchor('http://wiki.kohanaphp.com/', 'Documentation') ?>
+			<?php echo HTML::anchor('http://dev.kohanaphp.com/projects/kohana2/issues', 'Issues') ?>
+			<?php echo HTML::anchor('http://source.kohanaphp.com/svn/kohana2/tags/2.3.4', 'Repository') ?>
+		</p>
 
-	<h3 class="bottom">v2.4.x</h3>
-	<p>Current beta release of the v2.x branch.<br/>
-		<?php echo HTML::anchor('http://dev.kohanaphp.com/versions/show/10', 'Status') ?>
-		<?php echo HTML::anchor('http://dev.kohanaphp.com/projects/kohana2/wiki/24_Roadmap', 'Changes') ?>
-	</p>
-	<p>Download: <?php echo HTML::anchor('http://dev.kohanaphp.com/attachments/download/1349/kohana-2.4b2.zip', 'v2.4b2.zip') ?></p>
+		<h3 class="bottom">v2.4.0 <small class="warning">unstable</small><br/>
+			<small>Current beta release of the v2.x branch.</small></h3>
+		<p>
+			<?php echo HTML::anchor('http://dev.kohanaphp.com/attachments/download/1349/kohana-2.4b2.zip', 'Download (zip)') ?>
+			<?php echo HTML::anchor('http://dev.kohanaphp.com/versions/show/10', 'Status') ?>
+			<?php echo HTML::anchor('http://dev.kohanaphp.com/projects/kohana2/wiki/24_Roadmap', 'Changelog') ?>
+			<?php echo HTML::anchor('http://source.kohanaphp.com/svn/kohana2/trunk', 'Repository') ?>
+		</p>
 
+	</div>
 </div>
