ec1659c20da9444f915a5b6f4597b4b4e59045ba
Woody Gilk <woody.gilk@kohanaphp.com>
2010-04-24 18:12:13 +0200

    Added all the default modules, so that the user guide is complete

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -63,7 +63,12 @@ Kohana::$config->attach(new Kohana_Config_File);
  * Enable modules. Modules are referenced by a relative or absolute path.
  */
 Kohana::modules(array(
+	'auth'       => MODPATH.'auth',       // Basic authentication
+	'codebench'  => MODPATH.'codebench',  // Benchmarking tool
 	'database'   => MODPATH.'database',   // Database access
+	'image'      => MODPATH.'image',      // Image manipulation
+	'orm'        => MODPATH.'orm',        // Object Relationship Mapping
+	'pagination' => MODPATH.'pagination', // Paging of results
 	'userguide'  => MODPATH.'userguide',  // User guide and API documentation
 	));
 
