6169e43ae02b7bb18cb960e478b8632da917bd0f
Woody Gilk <woody.gilk@kohanaphp.com>
2010-04-27 18:12:18 +0200

    Updated donate page, we go live now!

--- application/classes/controller/donate.php
+++ application/classes/controller/donate.php
@@ -5,50 +5,7 @@ class Controller_Donate extends Controller_Website {
 	public function action_index()
 	{
 		$this->template->title   = 'Make A Donation';
-		$this->template->content = View::factory('donate')
-			->bind('post', $post)
-			->bind('errors', $errors);
-
-		$post = Validate::factory($_POST)
-			->filter('amount', 'trim')
-			->rules('amount', array(
-				'not_empty' => NULL,
-				'numeric'   => NULL,
-			))
-			->rules('using', array(
-				'not_empty' => NULL,
-				'in_array'  => array(array('paypal', 'google')),
-			));
-
-		if ($post->check())
-		{
-			// Format the amount into dollars
-			$post['amount'] = number_format($post['amount'], 2, '.', '');
-
-			// Return URL when payment is complete
-			$return = URL::site($this->request->uri, Request::$protocol);
-
-			// Process the payment using PayPal
-			$params = array
-			(
-				'cmd' => '_donations',
-				'item_name' => 'Donation for Kohana PHP Framework',
-				'currency_code' => 'USD',
-				'return' => $return,
-				'cancel_return' => $return,
-				'business' => 'alwayson@kohanaphp.com',
-			);
-
-			// Send the user to PayPal to complete their payment
-			$this->request->redirect('https://www.sandbox.paypal.com/cgi-bin/webscr?'.http_build_query($params));
-		}
-
-		if ( ! $_POST)
-		{
-			$post['amount'] = '10.00';
-		}
-
-		$errors = $post->errors('donate');
+		$this->template->content = View::factory('donate');
 	}
 
 } // End Donate
--- application/views/donate.php
+++ application/views/donate.php
@@ -4,22 +4,52 @@
 
 	<div class="span-12 suffix-2">
 		<h2>Make a Donation</h2>
-		<?php echo Form::open() ?>
+		<p>If possible, please use Google Checkout, as there are no fees for non-profit organizations, so more of your donation will be put to use.</p>
+		<div class="span-8">
+			<h6 class="top">Using Google Checkout</h6>
+			<script type="text/javascript">
+			function validateAmount(amount){
+				if(amount.value.match( /^[0-9]+(\.([0-9]+))?$/)){
+					return true;
+				}else{
+					alert('You must enter a valid donation.');
+					amount.focus();
+					return false;
+				}
+			}
+			</script>
+			<form action="https://checkout.google.com/cws/v2/Donations/622836985124940/checkoutForm" id="BB_BuyButtonForm" method="post" name="BB_BuyButtonForm" onSubmit="return validateAmount(this.item_price_1)" target="_top">
+				<input name="item_name_1" type="hidden" value="Kohana donation via Software Freedom Conservancy, Inc."/>
+				<input name="item_description_1" type="hidden" value="Donation to the Kohana project via its non-profit home, Software Freedom Conservancy, Inc.  10% of the donation will go to general operations of the Conservancy; 90% will be earmarked specifically for Kohana.  This is in accordance with the wishes of the Kohana project leadership."/>
+				<input name="item_quantity_1" type="hidden" value="1"/>
+				<input name="item_currency_1" type="hidden" value="USD"/>
+				<input name="item_is_modifiable_1" type="hidden" value="true"/>
+				<input name="item_min_price_1" type="hidden" value="5.0"/>
+				<input name="item_max_price_1" type="hidden" value="25000.0"/>
+				<input name="_charset_" type="hidden" value="utf-8"/>
 
-		<p><label>I would like to donate $<?php echo Form::input('amount', $post['amount'], array('size' => '6')) ?> to the Kohana PHP Framework.</label></p>
-		<?php if (isset($errors['amount'])): ?>
-		<p class="error">Please enter decimal value greater than <strong>1.00</strong>.</p>
-		<?php endif ?>
+				<table cellspacing="0">
+					<tr>
+						<td align="right" nowrap="nowrap" width="1%">&#x24; <input id="item_price_1" name="item_price_1" onfocus="this.style.color='black'; this.value='';" size="11" style="color:grey;" type="text" value="Enter Amount"/>
+						</td>
+						<td align="left" width="1%">
+							<input alt="Donate" src="https://checkout.google.com/buttons/donateNow.gif?merchant_id=622836985124940&amp;w=115&amp;h=50&amp;style=trans&amp;variant=text&amp;loc=en_US" type="image"/>
+						</td>
+					</tr>
+				</table>
+			</form>
+		</div>
+		<div class="span-4 last">
+			<h6 class="top">Using PayPal</h6>
+			<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+			<input type="hidden" name="cmd" value="_s-xclick">
+			<input type="hidden" name="hosted_button_id" value="TYCF8EKGYLL56">
+			<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+			<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+			</form>
+		</div>
 
-		<p>
-			<?php echo Form::button('using', HTML::image('media/img/google_checkout.png', array('alt' => 'Google Checkout')), array('value' => 'google')) ?>
-			<?php echo Form::button('using', HTML::image('media/img/paypal.png', array('alt' => 'PayPal')), array('value' => 'paypal')) ?><br/>
-			<small>If possible, please use Google Checkout, as there are no fees for non-profit organizations.</small>
-		</p>
-
-		<?php echo Form::close() ?>
-
-		<p><small>Your secure donation will be made to the <abbr title="Software Freedom Conservancy">SFC</abbr>. Ten percent (10%) of your donation will given to the <abbr title="Software Freedom Conservancy">SFC</abbr> to help cover administration costs and the remaining funds will be allocated to Kohana.</small></p>
+		<p class="clear"><small>Your secure donation will be made to the <abbr title="Software Freedom Conservancy">SFC</abbr>. Ten percent (10%) of your donation will given to the <abbr title="Software Freedom Conservancy">SFC</abbr> to help cover administration costs and the remaining funds will be allocated to Kohana.</small></p>
 	</div>
 	<div class="span-8 last">
 		<h2>Account Status</h2>
