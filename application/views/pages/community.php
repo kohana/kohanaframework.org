ebce88a39cf5841e741d8927d23584c5b79edada
joelpittet <joel@pittet.ca>
2010-06-24 08:34:02 +0200

    Added icons to the community page and changed list items into 3 columns

--- application/views/pages/community.php
+++ application/views/pages/community.php
@@ -1,30 +1,52 @@
-<div class="span-22 prefix-1 suffix-1 last">
+<div id="community" class="span-22 prefix-1 suffix-1 last">
 
 	<h1>Connect and Share</h1>
 
 	<p class="intro">An open source project is nothing without a community.</p>
 
 	<dl>
-		<dt>Forum</dt>
-		<dd>Official announcements, community support, and feedback can be found on our <?php echo HTML::anchor('http://forum.kohanaphp.com/', 'community forum') ?>.</dd>
+		<dt><?php echo HTML::anchor('http://forum.kohanaphp.com/', HTML::image('media/img/community_icon/forum.png', array('alt' => 'Forum'))) ?></dt>
+		<dd>
+			<h3><?php echo HTML::anchor('http://forum.kohanaphp.com/', 'Forum') ?></h3>
+			Official announcements, community support, and feedback can be found on our <?php echo HTML::anchor('http://forum.kohanaphp.com/', 'community forum') ?>.
+		</dd>
 
-		<dt>IRC</dt>
-		<dd>Chat with follow Kohana users at <?php echo HTML::anchor('irc://irc.freenode.net/kohana', '#kohana on freenode') ?>.</dd>
+		<dt><?php echo HTML::anchor('irc://irc.freenode.net/kohana', HTML::image('media/img/community_icon/irc.png', array('alt' => 'IRC'))) ?></dt>
+		<dd>
+			<h3><?php echo HTML::anchor('irc://irc.freenode.net/kohana', 'Forum') ?></h3>
+			Chat with follow Kohana users at <?php echo HTML::anchor('irc://irc.freenode.net/kohana', '#kohana on freenode') ?>.
+		</dd>
 
-		<dt>Github</dt>
-		<dd>The easiest way to contribute to v3.x is to <?php echo HTML::anchor('http://github.com/kohana', 'fork us on github') ?>.</dd>
+		<dt><?php echo HTML::anchor('http://github.com/kohana', HTML::image('media/img/community_icon/github.png', array('alt' => 'GitHub'))) ?></dt>
+		<dd>
+			<h3><?php echo HTML::anchor('http://github.com/kohana', 'Github') ?></h3>
+			The easiest way to contribute to v3.x is to <?php echo HTML::anchor('http://github.com/kohana', 'fork us on GitHub') ?>.
+		</dd>
 
-		<dt>Twitter</dt>
-		<dd>Read release announcements and random things about Kohana by <?php echo HTML::anchor('http://twitter.com/KohanaPHP', 'following us') ?>.</dd>
+		<dt><?php echo HTML::anchor('http://twitter.com/KohanaPHP', HTML::image('media/img/community_icon/twitter.png', array('alt' => 'Twitter'))) ?></dt>
+		<dd>
+			<h3><?php echo HTML::anchor('http://twitter.com/KohanaPHP', 'Twitter') ?></h3>
+			Read release announcements and random things about Kohana by <?php echo HTML::anchor('http://twitter.com/KohanaPHP', 'following us') ?>.
+		</dd>
 
-		<dt>Facebook</dt>
-		<dd>Join the <?php echo HTML::anchor('http://www.facebook.com/group.php?gid=140164501435', 'Facebook group') ?> and find other users.</dd>
+		<dt><?php echo HTML::anchor('http://www.facebook.com/group.php?gid=140164501435', HTML::image('media/img/community_icon/facebook.png', array('alt' => 'Facebook'))) ?></dt>
+		<dd>
+			<h3><?php echo HTML::anchor('http://www.facebook.com/group.php?gid=140164501435', 'Facebook') ?></h3>
+			Join the <?php echo HTML::anchor('http://www.facebook.com/group.php?gid=140164501435', 'Facebook group') ?> and find other users.
+		</dd>
 
-		<dt>Ohloh</dt>
-		<dd>View project information and give Kudos for <?php echo HTML::anchor('http://www.ohloh.net/p/ko3', 'v3.x') ?> and <?php echo HTML::anchor('http://www.ohloh.net/p/Kohana', 'v2.x') ?>.</dd>
+		<dt><?php echo HTML::anchor('http://stackoverflow.com/questions/tagged/kohana', HTML::image('media/img/community_icon/stackoverflow.png', array('alt' => 'Stack Overflow'))) ?></dt>
+		<dd>
+			<h3><?php echo HTML::anchor('http://stackoverflow.com/questions/tagged/kohana', 'Stack Overflow') ?></h3>
+			Share your knowledge by answering <?php echo HTML::anchor('http://stackoverflow.com/questions/tagged/kohana', 'user questions') ?> about Kohana.
+		</dd>
 
-		<dt>Stack Overflow</dt>
-		<dd>Share your knowledge by answering <?php echo HTML::anchor('http://stackoverflow.com/questions/tagged/kohana', 'user questions') ?> about Kohana.</dd>
+		<dt class="last"><?php echo HTML::anchor('http://www.ohloh.net/p/ko3', HTML::image('media/img/community_icon/ohloh.png', array('alt' => 'Ohloh'))) ?></dt>
+		<dd>
+			<h3><?php echo HTML::anchor('http://www.ohloh.net/p/ko3', 'Ohloh') ?></h3>
+			View project information and give Kudos for <?php echo HTML::anchor('http://www.ohloh.net/p/ko3', 'v3.x') ?> and <?php echo HTML::anchor('http://www.ohloh.net/p/Kohana', 'v2.x') ?>.
+		</dd>
+
 	</dl>
 
 </div>
