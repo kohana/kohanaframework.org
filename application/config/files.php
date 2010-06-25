b82daf7e70b6b64eb0f812ab2f78ccc68a8af274
Woody Gilk <woody.gilk@kohanaphp.com>
2010-06-25 12:38:30 -0500

    File name matters, not just ID

--- application/config/files.php
+++ application/config/files.php
@@ -5,7 +5,7 @@ return array(
 		'version'   => 'v3.0.6.2',
 		'codename'  => 'sumar hiti',
 		'status'    => 'stable',
-		'download'  => 'http://dev.kohanaframework.org/attachments/download/1569/kohana-3.0.6.zip',
+		'download'  => 'http://dev.kohanaframework.org/attachments/download/1569/kohana-3.0.6.2.zip',
 		'changelog' => 'http://dev.kohanaframework.org/projects/kohana3/issues?query_id=28',
 	),
 );
