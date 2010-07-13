85c2db295c33781fa07b70b4a084b533ee7d7fe2
Woody Gilk <woody.gilk@kohanaphp.com>
2010-07-12 20:23:00 -0500

    Updated download for 3.0.7

--- application/config/files.php
+++ application/config/files.php
@@ -2,10 +2,10 @@
 
 return array(
 	'kohana-latest' => array(
-		'version'   => 'v3.0.6.2',
-		'codename'  => 'sumar hiti',
+		'version'   => 'v3.0.7',
+		'codename'  => 'hattrick',
 		'status'    => 'stable',
-		'download'  => 'http://dev.kohanaframework.org/attachments/download/1569/kohana-3.0.6.2.zip',
-		'changelog' => 'http://dev.kohanaframework.org/projects/kohana3/issues?query_id=28',
+		'download'  => 'http://dev.kohanaframework.org/attachments/download/1588/kohana-3.0.7.zip',
+		'changelog' => 'http://dev.kohanaframework.org/projects/kohana3/issues?query_id=30',
 	),
 );
