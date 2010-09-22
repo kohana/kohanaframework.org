163e096686052c84de7e4e2dcf145ca95d87e3b2
Woody Gilk <woody.gilk@kohanaframework.org>
2010-09-22 14:41:29 -0500

    Updated files to point at the newest release

--- application/config/files.php
+++ application/config/files.php
@@ -2,10 +2,10 @@
 
 return array(
 	'kohana-latest' => array(
-		'version'   => 'v3.0.7',
-		'codename'  => 'hattrick',
+		'version'   => 'v3.0.8',
+		'codename'  => 'großen jäger',
 		'status'    => 'stable',
-		'download'  => 'http://dev.kohanaframework.org/attachments/download/1596/kohana-3.0.7.1.zip',
-		'changelog' => 'http://dev.kohanaframework.org/projects/kohana3/issues?query_id=30',
+		'download'  => 'http://dev.kohanaframework.org/attachments/download/1601/kohana-3.0.8.zip',
+		'changelog' => 'http://dev.kohanaframework.org/projects/kohana3/issues?query_id=32',
 	),
 );
