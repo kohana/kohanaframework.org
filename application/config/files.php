5667bf3d483cd20e2f9d04074707d8aaf665553b
Woody Gilk <woody.gilk@kohanaframework.org>
2010-12-30 14:32:14 -0600

    Setting latest download to 3.0.9

--- application/config/files.php
+++ application/config/files.php
@@ -2,10 +2,10 @@
 
 return array(
 	'kohana-latest' => array(
-		'version'   => 'v3.0.8',
-		'codename'  => 'großen jäger',
+		'version'   => 'v3.0.9',
+		'codename'  => 'nya året',
 		'status'    => 'stable',
-		'download'  => 'http://dev.kohanaframework.org/attachments/download/1601/kohana-3.0.8.zip',
-		'changelog' => 'http://dev.kohanaframework.org/projects/kohana3/issues?query_id=32',
+		'download'  => 'http://dev.kohanaframework.org/attachments/download/1628/kohana-3.0.9.zip',
+		'changelog' => 'http://dev.kohanaframework.org/projects/kohana3/issues?query_id=34',
 	),
 );
