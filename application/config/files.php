cbffde2adf8a7e2d4946f0b4a2a18bab2329bcc5
Jeremy Bush <contractfrombelow@gmail.com>
2011-05-06 09:47:52 -0500

    update downloads

--- application/config/files.php
+++ application/config/files.php
@@ -2,11 +2,11 @@
 
 return array(
 	'kohana-latest' => array(
-		'version'       => 'v3.1.3',
+		'version'       => 'v3.1.3.1',
 		'codename'      => 'araea',
 		'status'        => 'stable',
-		'download'      => 'http://dev.kohanaframework.org/attachments/download/1654/kohana-3.1.3.zip',
-		'changelog'     => 'http://dev.kohanaframework.org/projects/kohana3/issues?query_id=47',
+		'download'      => 'http://dev.kohanaframework.org/attachments/download/1656/kohana-3.1.3.1.zip',
+		'changelog'     => 'http://dev.kohanaframework.org/projects/kohana3/issues?query_id=49',
 		'documentation' => 'http://kohanaframework.org/3.1/guide/',
 	),
 	'kohana-3.0.x' => array(
