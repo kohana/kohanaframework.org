67cc0136b767abc84bdbc0adc8bce81ff438d4f1
Kiall Mac Innes <kiall@managedit.ie>
2011-07-26 21:53:17 +0100

    Clear out unused files from the old site.

--- application/views/template/feed.php
+++
@@ -1,8 +0,0 @@
-<ol class="feed" rel="<?php echo Route::get('feed')->uri(array('name' => $name)) ?>">
-<?php if (is_array($links)): ?>
-<?php foreach ($links as $link => $title): ?>
-	<li><?php echo HTML::anchor($link, $title) ?></li>
-<?php endforeach ?>
-<?php endif ?>
-	<li class="more">&raquo; <?php echo HTML::anchor($feed['link'], $feed['title']) ?></li>
-</ol>
