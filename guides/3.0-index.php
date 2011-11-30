5b7477e4861f838e6a26de3eea01235bc5780b7e
Kiall Mac Innes <kiall@managedit.ie>
2011-11-30 21:32:54 +0000

    PagodaBox Userguide Changes

--- guides/3.0-index.php
+++ guides/3.0-index.php
@@ -1,47 +1,33 @@
 <?php
-/**
- * Detect and "route" to guide applications...
- */
-if (preg_match('/^\/([\d\.]+)\/guide/', $_SERVER['REQUEST_URI'], $matches))
-{
-	$version = $matches[1];
-
-	if (is_file('../guides/'.$version.'-index.php'))
-	{
-		return include '../guides/'.$version.'-index.php';
-	}
-}
-
-unset($matches);
 
 /**
  * The directory in which your application specific resources are located.
- * The application directory must contain the config/kohana.php file.
+ * The application directory must contain the bootstrap.php file.
  *
- * @see  http://docs.kohanaphp.com/install#application
+ * @see  http://kohanaframework.org/guide/about.install#application
  */
-$application = '../application';
+$application = '3.0/application';
 
 /**
  * The directory in which your modules are located.
  *
- * @see  http://docs.kohanaphp.com/install#modules
+ * @see  http://kohanaframework.org/guide/about.install#modules
  */
-$modules = '../modules';
+$modules = '3.0/modules';
 
 /**
  * The directory in which the Kohana resources are located. The system
  * directory must contain the classes/kohana.php file.
  *
- * @see  http://docs.kohanaphp.com/install#system
+ * @see  http://kohanaframework.org/guide/about.install#system
  */
-$system = '../system';
+$system = '3.0/system';
 
 /**
  * The default extension of resource files. If you change this, all resources
  * must be renamed to use the new extension.
  *
- * @see  http://docs.kohanaphp.com/install#ext
+ * @see  http://kohanaframework.org/guide/about.install#ext
  */
 define('EXT', '.php');
 
@@ -54,6 +40,9 @@ define('EXT', '.php');
  *
  * In a production environment, it is safe to ignore notices and strict warnings.
  * Disable them by using: E_ALL ^ E_NOTICE
+ *
+ * When using a legacy application with PHP >= 5.3, it is recommended to disable
+ * deprecated notices. Disable with: E_ALL & ~E_DEPRECATED
  */
 error_reporting(E_ALL | E_STRICT);
 
@@ -61,23 +50,29 @@ error_reporting(E_ALL | E_STRICT);
  * End of standard configuration! Changing any of the code below should only be
  * attempted by those with a working knowledge of Kohana internals.
  *
- * @see  http://docs.kohanaphp.com/bootstrap
+ * @see  http://kohanaframework.org/guide/using.configuration
  */
 
 // Set the full path to the docroot
 define('DOCROOT', realpath(dirname(__FILE__)).DIRECTORY_SEPARATOR);
 
-// Make the application relative to the docroot
+// Make the application relative to the docroot, for symlink'd index.php
 if ( ! is_dir($application) AND is_dir(DOCROOT.$application))
+{
 	$application = DOCROOT.$application;
+}
 
-// Make the modules relative to the docroot
+// Make the modules relative to the docroot, for symlink'd index.php
 if ( ! is_dir($modules) AND is_dir(DOCROOT.$modules))
+{
 	$modules = DOCROOT.$modules;
+}
 
-// Make the system relative to the docroot
+// Make the system relative to the docroot, for symlink'd index.php
 if ( ! is_dir($system) AND is_dir(DOCROOT.$system))
+{
 	$system = DOCROOT.$system;
+}
 
 // Define the absolute paths for configured directories
 define('APPPATH', realpath($application).DIRECTORY_SEPARATOR);
@@ -87,21 +82,28 @@ define('SYSPATH', realpath($system).DIRECTORY_SEPARATOR);
 // Clean up the configuration vars
 unset($application, $modules, $system);
 
-// Define the start time of the application
-define('KOHANA_START_TIME', microtime(TRUE));
+if (file_exists('install'.EXT))
+{
+	// Load the installation check
+	return include 'install'.EXT;
+}
+
+// Load the base, low-level functions
+require SYSPATH.'base'.EXT;
+
+// Load the core Kohana class
+require SYSPATH.'classes/kohana/core'.EXT;
+
+if (is_file(APPPATH.'classes/kohana'.EXT))
+{
+	// Application extends the core
+	require APPPATH.'classes/kohana'.EXT;
+}
+else
+{
+	// Load empty core extension
+	require SYSPATH.'classes/kohana'.EXT;
+}
 
 // Bootstrap the application
-require APPPATH.'bootstrap'.EXT;
-
-
-
-/**
- * Execute the main request. A source of the URI can be passed, eg: $_SERVER['PATH_INFO'].
- * If no source is specified, the URI will be automatically detected.
- */
-$cache = (extension_loaded('apc')) ? HTTP_Cache::factory('apc') : NULL;
-
-echo Request::factory(TRUE, $cache)
-	->execute()
-	->send_headers()
-	->body();
+require '3.0-bootstrap'.EXT;
