ab880dfd6de42c13d32d7d486a509b46ad635ada
Jeremy Bush <contractfrombelow@gmail.com>
2012-08-30 17:23:01 -0500

    Updates for 3.2.2 release

--- application/config/files.php
+++ application/config/files.php
@@ -2,13 +2,13 @@
 
 return array(
 	'kohana-latest' => array(
-		'version'       => 'v3.2.1',
-		'codename'      => 'Milvago',
+		'version'       => 'v3.2.2',
+		'codename'      => 'hypoleucos',
 		'status'        => 'stable',
-		'download'      => 'http://dev.kohanaframework.org/attachments/download/1703/kohana-3.2.1.zip',
-		'changelog'     => 'http://dev.kohanaframework.org/projects/kohana3/issues?query_id=60',
+		'download'      => 'https://github.com/downloads/kohana/kohana/kohana-3.2.2.zip',
+		'changelog'     => 'http://dev.kohanaframework.org/projects/kohana3/issues?query_id=61',
 		'documentation' => 'http://kohanaframework.org/3.2/guide/',
-		'issues'        => 'http://dev.kohanaframework.org/projects/kohana3/issues?query_id=61',
+		'issues'        => 'http://dev.kohanaframework.org/projects/kohana3/issues?query_id=62',
 		'support_until' => 'March, 2012'
 	),
 	'support' => array(
